f4005f40000221d4825f27fa7dc69a65e5e25141
Ivan Filho <ivanfilho21@gmail.com>
2020-09-23 17:59:29 -0300

    Implementar metodos unauthorized, login, logout, refresh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,25 +10,33 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+
+    private $response = ['error' => false];
+    private $returnCode = 200;
+
     function __construct()
     {
         $this->middleware('auth:api', [
             'except' => ['login', 'create', 'unauthorized']
         ]);
     }
+    
+    function unauthorized()
+    {
+        $this->response['error'] = __('auth.unauthorized');
+        $this->returnCode = 401;
+        return $this->response();
+    }
 
     function create(Request $request)
     {
-        $response = ['errors' => false];
-        $returnCode = 200;
-
         $data = $request->only('name', 'email', 'password', 'password_confirmation');
         $validation = $this->validation($data);
 
         if ($validation->fails()) {
-            $response['errors'] = $validation->errors();
-            $returnCode = 422;
-            return response()->json($response, $returnCode);
+            $this->response['error'] = $validation->errors();
+            $this->returnCode = 422;
+            return $this->response();
         }
 
         $user = User::create([
@@ -37,30 +45,43 @@ class AuthController extends Controller
             'password' => Hash::make($data['password']),
         ]);
 
-        $token = auth()->attempt([
-            'email' => $data['email'],
-            'password' => $data['password'],
-        ]);
+        $token = $this->attempt($request);
 
         if (! $token) {
-            $response['errors'] = true;
-            $returnCode = 500;
-            return response()->json($response, $returnCode);
+            $this->response['error'] = true;
+            $this->returnCode = 500;
+            return $this->response();
         }
 
-        $response['token'] = $token;
-        
-        return response()->json($response, $returnCode);
+        $this->response['token'] = $token;
+        return $this->response();
     }
     
-    function login()
-    {}
+    function login(Request $request)
+    {
+        $token = $this->attempt($request);
+
+        if (! $token) {
+            $this->response['error'] = __('auth.failed');
+            $this->returnCode = 400;
+            return $this->response();
+        }
+        
+        $this->response['token'] = $token;
+        return $this->response();
+    }
     
     function logout()
-    {}
-    
-    function unauthorized()
-    {}
+    {
+        auth()->logout();
+        return $this->response();
+    }
+
+    function refresh()
+    {
+        $this->response['token'] = auth()->refresh();
+        return $this->response();
+    }
 
     private function validation($data)
     {
@@ -69,6 +90,19 @@ class AuthController extends Controller
             'email' => ['required', 'email', 'max:100'],
             'password' => ['required', 'min:4', 'confirmed'],
         ]);
+    }
+
+    private function attempt(Request $request)
+    {
+        return auth()->attempt([
+            'email' => $request->input('email', ''),
+            'password' => $request->input('password', ''),
+        ]);
+    }
+
+    private function response()
+    {
+        return response()->json($this->response, $this->returnCode);
     }
 
 }
