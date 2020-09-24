8f7723b11ebb779bf0650cad41c50dd4d90669bd
Ivan Filho <ivanfilho21@gmail.com>
2020-09-24 09:02:10 -0300

    Implementar view e list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\User;
 
 class UserController extends Controller
 {
     
-    function view($id)
+    function view($id = 0)
     {
+        $user = User::find($id);
+
+        if (! $user) {
+            $this->response['error'] = __('auth.failed');
+            $this->returnCode = 400;
+            return $this->response();
+        }
+
+        $this->response['user'] = $this->userData($user);
         return $this->response();
     }
 
@@ -22,18 +32,24 @@ class UserController extends Controller
             return $this->response();
         }
 
-        $this->response['user'] = [
-            'name' => $user->name,
-            'email' => $user->email,
-            'created_at' => $user->created_at,
-            'updated_at' => $user->updated_at,
-        ];
-
+        $this->response['user'] = $this->userData($user, true);
         return $this->response();
     }
 
     function list()
     {
+        $users = User::all();
+
+        if (! count($users)) {
+            $this->response['error'] = __('auth.500');
+            $this->returnCode = 500;
+            return $this->response();
+        }
+
+        foreach ($users as $user) {
+            $this->response['users'][] = $this->userData($user);
+        }
+
         return $this->response();
     }
 
@@ -45,6 +61,20 @@ class UserController extends Controller
     function delete()
     {
         return $this->response();
+    }
+
+    private function userData($user)
+    {
+        $id = $this->loggedUser ? $this->loggedUser->id : null;
+        $current = $user->id == $id;
+
+        return [
+            'name' => $user->name,
+            'email' => $user->email,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at,
+            'current_user' => $current,
+        ];
     }
 
 }
