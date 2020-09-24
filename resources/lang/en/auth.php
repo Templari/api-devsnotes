93520b3c2cabc0562469aa229ccbf87097d09023
Ivan Filho <ivanfilho21@gmail.com>
2020-09-24 08:39:45 -0300

    Adicionar novo valor de lang

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,5 +16,6 @@ return [
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'unauthorized' => 'You do not have permission to make this request.',
+    '500' => 'Something went wrong.',
 
 ];
