6d8c7f7303ac8742051fbb55aca0535777e92e1b
Ivan Filho <ivanfilho21@gmail.com>
2020-09-24 11:23:44 -0300

    Renomear controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\User;
+
+class LoginController extends Controller
+{
+
+    function __construct()
+    {
+        $this->middleware('auth:api', [
+            'except' => ['login', 'unauthorized']
+        ]);
+    }
+    
+    function unauthorized()
+    {
+        return $this->response(401, __('auth.unauthorized'));
+    }
+    
+    function login(Request $request)
+    {
+        $token = $this->attempt($request);
+
+        if (! $token) {
+            return $this->response(400, __('auth.failed'));
+        }
+        
+        $this->response['token'] = $token;
+        return $this->response();
+    }
+    
+    function logout()
+    {
+        auth()->logout();
+        return $this->response();
+    }
+
+    function refresh()
+    {
+        $this->response['token'] = auth()->refresh();
+        return $this->response();
+    }
+
+    private function attempt(Request $request)
+    {
+        return auth()->attempt([
+            'email' => $request->input('email', ''),
+            'password' => $request->input('password', ''),
+        ]);
+    }
+
+}
