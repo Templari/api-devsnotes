72f1db998533da791e771b87594c04f7ca13d411
Ivan Filho <ivanfilho21@gmail.com>
2020-09-24 08:39:33 -0300

    Implementar metodo viewCurrent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,18 +8,43 @@ class UserController extends Controller
 {
     
     function view($id)
-    {}
+    {
+        return $this->response();
+    }
 
     function viewCurrent()
-    {}
+    {
+        $user = $this->loggedUser;
+
+        if (! $user) {
+            $this->response['error'] = __('auth.500');
+            $this->returnCode = 500;
+            return $this->response();
+        }
+
+        $this->response['user'] = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at,
+        ];
+
+        return $this->response();
+    }
 
     function list()
-    {}
+    {
+        return $this->response();
+    }
 
     function update()
-    {}
+    {
+        return $this->response();
+    }
 
     function delete()
-    {}
+    {
+        return $this->response();
+    }
 
 }
