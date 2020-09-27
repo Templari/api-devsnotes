5def9f9bef6253f0796440f5b3a569ca7fe249ca
Ivan Filho <ivanfilho21@gmail.com>
2020-09-27 11:58:43 -0300

    Remover metodo e mover para Controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -46,12 +46,4 @@ class LoginController extends Controller
         return $this->response();
     }
 
-    private function attempt(Request $request)
-    {
-        return auth()->attempt([
-            'email' => $request->input('email', ''),
-            'password' => $request->input('password', ''),
-        ]);
-    }
-
 }
