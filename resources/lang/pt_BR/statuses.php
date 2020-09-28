abc19a0098b762140d8ba8b89bdb269bde15da13
Ivan Filho <ivanfilho21@gmail.com>
2020-09-28 10:18:20 -0300

    Criar arquivo de lang

---
+++ resources/lang/pt_BR/statuses.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    '400_notes' => 'Nota não existe em nossos registros.',
+    '401' => 'Usuário não tem permissão para executar esta ação.',
+    '500' => 'Algo deu errado. Tente novamente mais tarde.',
+];
